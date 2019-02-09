850d2a66f461678ee890e7af8ba6b98e3ac982d3
hassenH <hadhri-h@live.fr>
2019-02-09 16:03:20 +0100

    Ajout Exercice 9

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $('#firstText, #thirdText').css('border','5px green dashed'); // Avec le sélecteur multiple ("sélecteur1, sélecteur2, sélecteurN") je vais sélectionner les id dont je vais appliquer des bordures
+});
